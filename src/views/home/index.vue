ad89bccf27ccd78c90812b2050a0996747a6c6e1
sulihua <l1156832448@qq.com>
2020-07-17 14:57:53 +0800

    项目初始化

---
+++ src/views/home/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='home-container'>
+      home
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'></style>
